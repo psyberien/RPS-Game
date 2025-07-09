d374a62017776f4524a076794e6c3a2e5b7c1600
saak <31166582+psyberien@users.noreply.github.com>
2025-07-10 02:14:02 +0800

    Build play round function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
 let getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
     let chosenOption;
@@ -14,7 +20,38 @@ let getComputerChoice = () => {
 }
 
 
-let getHumanChoice = () =>{
+let getHumanChoice = () => {
     let choice = prompt("Enter your choice! ");
+    choice = choice.toLowerCase();
     return choice;
 }
+
+let playRound  = (humanChoice, computerChoice) => {
+  if(humanChoice === "rock" && computerChoice === "paper"){
+    console.log("You Lose! paper beats rock " + computerChoice);
+    computerScore++;
+  } else if(humanChoice === "rock" && computerChoice === "scizor"){
+    console.log("You Win! rock beats scizor");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scizor"){
+    console.log("You Lose! scizor beats paper");
+    computerScore++;
+  } else if(humanChoice === "paper" && computerChoice === "rock"){
+    console.log("You Win! paper beats rock");
+    humanScore++;
+  } else if (humanChoice === "scizor" && computerChoice === "rock"){
+    console.log("You Lose! rock beat scizor");
+    computerScore++;
+  } else if(humanChoice === "scizor" && computerChoice === "paper"){
+     console.log("You Win! scizor beats paper")
+      humanScore++;
+  } else {
+    console.log("DRAW");
+  }
+} 
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
